7807f7fc1d8a9f689644a895c1471fe430099b4c
Ryan Delaney <ryan.delaney@gmail.com>
2020-03-26 13:03:50 -0700

    Add fixtures for test functions

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,11 +1,26 @@
 import pytest as _pytest
 
-import functions.aws as aws
-from functions.aws import Boto3InputError
+from functions.aws import Boto3Error, Boto3InputError, Boto3Result
 
 
 class TestException(Exception):
     pass
+
+
+@_pytest.fixture
+def test_function():
+    def foo(string):
+        return f"your string was {string}"
+
+    return foo
+
+
+@_pytest.fixture
+def test_exception_raiser():
+    def foo():
+        raise Boto3Error
+
+    return foo
 
 
 @_pytest.fixture
@@ -15,7 +30,7 @@ def generic_mock(mocker):
 
 @_pytest.fixture
 def result_with_body(mocker):
-    return aws.Boto3Result(
+    return Boto3Result(
         response={"ResponseMetadata": {"HTTPStatusCode": "200"}, "foo": "bar"}
     )
 
@@ -25,7 +40,7 @@ def result_with_exception(mocker):
     try:
         raise TestException
     except TestException as te:
-        return aws.Boto3Result(response=None, exc=te)
+        return Boto3Result(response=None, exc=te)
 
 
 class TestBoto3Result:
@@ -50,7 +65,7 @@ class TestBoto3Result:
 
     def test_Boto3Result_with_neither(self):
         with _pytest.raises(Boto3InputError):
-            aws.Boto3Result()
+            Boto3Result()
 
 
 class TestInvoke:
