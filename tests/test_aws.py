637dba707c677c21777591b218d7e9ffc4c3840c
Ryan Delaney <ryan.delaney@gmail.com>
2020-03-26 18:10:05 -0700

    Move test fixture definitions into conftest.py

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,47 +1,5 @@
 import pytest as _pytest
 
-from functions.aws import Boto3Error, Boto3InputError, Boto3Result
-
-
-class TestException(Exception):
-    pass
-
-
-@_pytest.fixture
-def test_function():
-    def foo(string):
-        return f"your string was {string}"
-
-    return foo
-
-
-@_pytest.fixture
-def test_exception_raiser():
-    def foo():
-        raise Boto3Error
-
-    return foo
-
-
-@_pytest.fixture
-def generic_mock(mocker):
-    return mocker.MagicMock
-
-
-@_pytest.fixture
-def result_with_body(mocker):
-    return Boto3Result(
-        response={"ResponseMetadata": {"HTTPStatusCode": "200"}, "foo": "bar"}
-    )
-
-
-@_pytest.fixture
-def result_with_exception(mocker):
-    try:
-        raise TestException
-    except TestException as te:
-        return Boto3Result(response=None, exc=te)
-
 
 class TestBoto3Result:
     def test_Boto3Result_with_body(self, result_with_body):
@@ -53,19 +11,21 @@ class TestBoto3Result:
         assert result_with_body.exc is None
         assert result_with_body.error == {}
 
-    def test_Boto3Result_with_exception(self, result_with_exception):
+    def test_Boto3Result_with_exception(
+        self, result_with_exception, test_exception
+    ):
         assert result_with_exception.status is None
         assert result_with_exception.body is None
-        assert isinstance(result_with_exception.exc, TestException)
+        assert isinstance(result_with_exception.exc, test_exception)
         assert isinstance(result_with_exception.error, dict)
         assert all(
             key in result_with_exception.error
             for key in ("title", "message", "traceback")
         )
 
-    def test_Boto3Result_with_neither(self):
-        with _pytest.raises(Boto3InputError):
-            Boto3Result()
+    def test_Boto3Result_with_neither(self, inputerror_exception, boto3result):
+        with _pytest.raises(inputerror_exception):
+            boto3result()
 
 
 class TestInvoke:
