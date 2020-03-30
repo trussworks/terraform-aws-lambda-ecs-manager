1f6ada2e5039334c5f4e4e6353c9e89ee209c786
Ryan Delaney <ryan.delaney@gmail.com>
2020-03-30 11:16:27 -0700

    Revert "Always include status in the response body"
    This reverts commit 3fc8cefdc9b516bc878391a7977c390476bef616.

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -5,11 +5,8 @@ class TestBoto3Result:
     def test_Boto3Result_with_body(self, result_with_body):
         assert result_with_body.status == "200"
         assert result_with_body.body == {
-            "status": "200",
-            "response": {
-                "ResponseMetadata": {"HTTPStatusCode": "200"},
-                "foo": "bar",
-            },
+            "ResponseMetadata": {"HTTPStatusCode": "200"},
+            "foo": "bar",
         }
         assert result_with_body.exc is None
         assert result_with_body.error == {}
@@ -18,10 +15,7 @@ class TestBoto3Result:
         self, result_with_exception, test_exception
     ):
         assert result_with_exception.status is None
-        assert result_with_exception.body == {
-            "status": None,
-            "response": {},
-        }
+        assert result_with_exception.body == {}
         assert isinstance(result_with_exception.exc, test_exception)
         assert isinstance(result_with_exception.error, dict)
         assert all(
