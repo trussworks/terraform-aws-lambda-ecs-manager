253842493ddd213a63bce6002e523466dfc2fc31
Ryan Delaney <ryan@truss.works>
2020-03-31 16:25:55 -0700

    Revoke UpdateService, grant RunTask

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ data "aws_iam_policy_document" "main" {
       "ecs:DescribeServices",
       "ecs:DescribeTaskDefinition",
       "ecs:RegisterTaskDefinition",
-      "ecs:UpdateService",
+      "ecs:RunTask",
     ]
 
     resources = ["*"]
