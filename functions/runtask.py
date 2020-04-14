8e84addb9997d7ee49de3b5ecdfe4049a3f4b88c
Ryan Delaney <ryan@truss.works>
2020-04-14 13:40:20 -0700

    If there was an exception then return an error

--- functions/runtask.py
+++ functions/runtask.py
@@ -283,7 +283,10 @@ def lambda_handler(
     response = {"request_payload": command}
 
     result = _runtask(command)
-    response.update(result.body)
+    if result.exc:
+        response.update(result.error)
+    else:
+        response.update(result.body)
 
     duration = "{} ms".format(round(1000 * (time.time() - start_t), 2))
     log(
