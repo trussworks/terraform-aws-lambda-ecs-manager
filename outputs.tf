a4f7c7075b90b004d1adb66444a1fcc3dfd8710b
Ryan Delaney <ryan@truss.works>
2020-04-28 12:09:59 -0700

    Add various outputs from the lambda_function provider

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,33 @@ output "log_group" {
   description = "CloudWatch log group the lambda logs to."
   value       = local.log_group
 }
+
+output "version" {
+  description = "Published version of the lambda function."
+  value       = aws_lambda_function.main.version
+}
+
+output "qualified_arn" {
+  description = "The Amazon Resource Name (ARN) identifying your Lambda Function Version (if versioning is enabled via publish = true)."
+  value       = aws_lambda_function.main.qualified_arn
+}
+
+output "name" {
+  description = "Name of the lambda function."
+  value       = var.app
+}
+
+output "last_modified" {
+  description = "The date this resource was last modified."
+  value       = aws_lambda_function.main.last_modified
+}
+
+output "source_code_hash" {
+  description = "Base64-encoded representation of raw SHA-256 sum of the zip file."
+  value       = aws_lambda_function.main.source_code_hash
+}
+
+output "source_code_size" {
+  description = "The size in bytes of the function .zip file."
+  value       = aws_lambda_function.main.source_code_size
+}
