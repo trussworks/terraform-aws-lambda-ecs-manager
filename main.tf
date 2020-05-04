3601d5c3a432ba09ac83263e09c4f919b801beda
Ryan Delaney <ryan@truss.works>
2020-05-04 13:56:40 -0700

    Rename lambda handler to runtask
    This corresponds to the previous commit, which renamed the module from
    manager back to runtask.

--- main.tf
+++ main.tf
@@ -136,7 +136,7 @@ resource "aws_lambda_function" "main" {
   description   = "Updates an ECS service"
 
   role             = aws_iam_role.main.arn
-  handler          = "manager.lambda_handler"
+  handler          = "runtask.lambda_handler"
   source_code_hash = data.archive_file.main.output_base64sha256
   runtime          = "python3.7"
   timeout          = 120
