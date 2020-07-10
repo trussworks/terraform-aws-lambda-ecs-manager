3d850310d4ed7cb82439451c615dad36340cb160
Ryan Delaney <ryan@truss.works>
2020-07-10 11:41:46 -0400

    Change logging level from WARN to INFO
    Also add tests.

--- functions/manager.py
+++ functions/manager.py
@@ -15,7 +15,7 @@ from typing import Any, Dict, List, Optional, Union, cast
 import boto3
 
 LOGGER = logging.getLogger()
-LOGGER_LEVEL = logging.WARN
+LOGGER_LEVEL = logging.INFO
 LOGGER.setLevel(LOGGER_LEVEL)
 STDOUT_HANDLER = logging.StreamHandler(sys.stdout)
 STDOUT_HANDLER.setLevel(logging.INFO)
