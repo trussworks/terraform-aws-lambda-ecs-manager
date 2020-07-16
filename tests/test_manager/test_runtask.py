e54b574a904b8807e859b89428ca1453f2c639e9
Ryan Delaney <ryan@truss.works>
2020-07-16 14:40:36 -0400

    Test runtask checking for required keys

--- tests/test_manager/test_runtask.py
+++ tests/test_manager/test_runtask.py
@@ -1,4 +1,12 @@
+import itertools
+
+import pytest as _pytest
+
 import functions.manager as manager
+from functions.manager import Boto3Result as Boto3Result
+
+required_keys = ["container_id", "service_id", "cluster_id"]
+required_keys_combinations = itertools.combinations(required_keys, 2)
 
 
 class TestValidations:
@@ -8,3 +16,17 @@ class TestValidations:
         assert isinstance(result.exc, TypeError)
         assert result.error is not None
         assert result.body == {}
+
+    @_pytest.mark.parametrize(
+        ("body"),
+        [
+            {combo[0]: "foo", combo[1]: "bar"}
+            for combo in required_keys_combinations
+        ],
+    )
+    def test_required_keys_present(mock_invoke, body):
+        return
+        result = manager._runtask(body)
+
+        assert isinstance(result, Boto3Result)
+        assert result.exc == KeyError(manager._missing_required_keys())
