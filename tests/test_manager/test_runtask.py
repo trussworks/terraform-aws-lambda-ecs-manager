f565f2ec904092fb6ff52b2ca4d6ec1f91ac8d2c
Ryan Delaney <ryan@truss.works>
2020-07-16 15:16:19 -0400

    Test critical log message emitted

--- tests/test_manager/test_runtask.py
+++ tests/test_manager/test_runtask.py
@@ -1,4 +1,5 @@
 import itertools
+import logging
 
 import pytest as _pytest
 
@@ -24,10 +25,13 @@ class TestValidations:
             for combo in required_keys_combinations
         ],
     )
-    def test_required_keys_present(mock_invoke, body):
+    def test_required_keys_present(mock_invoke, body, caplog):
         result = manager._runtask(body)
 
         assert isinstance(result, Boto3Result)
         assert isinstance(result.exc, KeyError)
         assert result.error is not None
         assert result.body == {}
+
+        assert len(caplog.records) == 1
+        assert caplog.records[0].levelno == logging.CRITICAL
